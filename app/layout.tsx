d630dbe029bf3fb02065fe4c4aadcd2d28f8c332
Vithushan <63534975+ACuteWoof@users.noreply.github.com>
2023-10-07 00:45:13 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,14 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-B1V8D3CWHF"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-B1V8D3CWHF');
-</script>
       <body className={roboto_mono.className}>{children}</body>
     </html>
   )
