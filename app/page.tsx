03b6e342e6649ce814a5935391019dcac1a83609
Vithushan <63534975+ACuteWoof@users.noreply.github.com>
2023-10-06 22:45:26 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,28 @@ import Hero from "./sections/hero";
 import Gallery from "./sections/gallery"
 import Link from "next/link";
 
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBbD8Jx476YEbceQTUq3CK5-VceaTTnmuw",
+    authDomain: "obsek-gallery.firebaseapp.com",
+    projectId: "obsek-gallery",
+    storageBucket: "obsek-gallery.appspot.com",
+    messagingSenderId: "384822949140",
+    appId: "1:384822949140:web:ae4c0d1bcf244aa4ccfe01",
+    measurementId: "G-B1V8D3CWHF"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-cinder-950 overflow-x-hidden">
