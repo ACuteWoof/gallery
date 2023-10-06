043ad3caf5af2e5aa74578c8bf105191be8616c1
Vithushan <63534975+ACuteWoof@users.noreply.github.com>
2023-10-06 23:46:07 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,6 @@ import Hero from "./sections/hero";
 import Gallery from "./sections/gallery"
 import Link from "next/link";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics, isSupported } from "firebase/analytics";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBbD8Jx476YEbceQTUq3CK5-VceaTTnmuw",
-  authDomain: "obsek-gallery.firebaseapp.com",
-  projectId: "obsek-gallery",
-  storageBucket: "obsek-gallery.appspot.com",
-  messagingSenderId: "384822949140",
-  appId: "1:384822949140:web:ae4c0d1bcf244aa4ccfe01",
-  measurementId: "G-B1V8D3CWHF"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
 
 export default function Home() {
   return (
