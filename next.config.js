65e6bd736f1ca3d2d1f9c4c320f35fe58f4159bc
Vithushan <63534975+ACuteWoof@users.noreply.github.com>
2023-10-07 09:01:39 +0000

    config for vercel

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
-const debug = process.env.NODE_ENV !== "production";
 
 const nextConfig = {
-  output: "export",
   images: {
-    unoptimized: true,
     minimumCacheTTL: 60,
   },
-  basePath: !debug ? "/gallery" : "",
-  assetPrefix: !debug ? "/gallery/" : "",
 };
 
 module.exports = nextConfig;
