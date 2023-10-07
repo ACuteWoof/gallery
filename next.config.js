b66d69a1da537bd9d1da95ffac6830fbfdaaf551
Vithushan <63534975+ACuteWoof@users.noreply.github.com>
2023-10-07 09:12:18 +0000

    for github

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
 
 const nextConfig = {
   images: {
+    unoptimized: true,
     minimumCacheTTL: 60,
   },
+  basePath: !debug ? "/gallery" : "",
+  assetPrefix: !debug ? "/gallery/" : "",
 };
 
 module.exports = nextConfig;
